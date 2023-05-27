6454bb39f513c933931568cb276c9351381cd55d
unknown <seunghochoe12@gmail.com>
2023-05-27 11:55:15 +0900

    안녕하세요

---
+++ Project1/main.cpp
@@ -0,0 +1,37 @@
+
+
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	printf("My age: %d \n", 20);
+//	printf("%d is my point \m", 100);
+//	printf("Good \nmorning \neverybody\n");
+//	return 0;
+//
+//}
+
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	printf("hello everybody\n");
+//	printf("%d\n", 1234);
+//	printf("%d %d\n", 10, 20);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main(void)
+//{
+//	printf("제이름은 최승호 입니다. \n");
+//	printf("제나이는 %d 입니다. \n",16);
+//	printf("제사는곳은 평택시입니다. \n");
+//}
+
+//#include <stdio.h>
+//int main(void)
+//{
+//	printf("%d x %d = %d",4,5,4*5);
+//	return 0;
+//}
