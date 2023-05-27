1881a6cb19a86d92b569c20cb46429278065960e
unknown <seunghochoe12@gmail.com>
2023-05-27 12:54:36 +0900

    12:54

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,37 +1,20 @@
 
+#include <stdio.h>
 
-//#include <stdio.h>
-//
-//int main(void)
-//{
-//	printf("My age: %d \n", 20);
-//	printf("%d is my point \m", 100);
-//	printf("Good \nmorning \neverybody\n");
-//	return 0;
-//
-//}
+int main(void)
+{
+	int num1 = 12;
+	int num2 = 12;
 
-//#include <stdio.h>
-//
-//int main(void)
-//{
-//	printf("hello everybody\n");
-//	printf("%d\n", 1234);
-//	printf("%d %d\n", 10, 20);
-//	return 0;
-//}
 
-//#include <stdio.h>
-//int main(void)
-//{
-//	printf("제이름은 최승호 입니다. \n");
-//	printf("제나이는 %d 입니다. \n",16);
-//	printf("제사는곳은 평택시입니다. \n");
-//}
+	printf("num1: %d \n", num1);
+	printf("num1++: %d \n", num1++);
+	printf("num1: %d \n\n", num1);
 
-//#include <stdio.h>
-//int main(void)
-//{
-//	printf("%d x %d = %d",4,5,4*5);
-//	return 0;
-//}
+
+	printf("num2: %d \n", num2);
+	printf("++num2: %d \n", ++num2);
+	printf("num2: %d \n", num2);
+
+	return 0;
+}
